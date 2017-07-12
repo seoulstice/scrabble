10e858b88765d8cb4acc134ecb29f060d093898c
seoulstice <jungyoungjoon@gmail.com>
2017-07-12 10:00:27 -0600

    Work on Scrabble

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,11 +1,21 @@
 class Scrabble
+  require 'pry'
 
-  def score(word)
-    1
+  def score("word")
+    skip
   end
 
+  def word_split("word")
+    letters = word.split(//)
+    letters.each do |letter|
+    end
+    puts letter
+  end
+
+  # need score_with_multipliers method
+
   def point_values
-    {
+    hash = {
       "A"=>1, "B"=>3, "C"=>3, "D"=>2,
       "E"=>1, "F"=>4, "G"=>2, "H"=>4,
       "I"=>1, "J"=>8, "K"=>5, "L"=>1,
@@ -14,5 +24,8 @@ class Scrabble
       "U"=>1, "V"=>4, "W"=>4, "X"=>8,
       "Y"=>4, "Z"=>10
     }
+
   end
+  binding.pry
+
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -5,9 +5,20 @@ require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
+  skip
+  def test_it_exists
+    scrabble = Scrabble.new
+  end
+
+
   def test_it_can_score_a_single_letter
     skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
+  end
+
+  def test_it_splits_word_into_array
+    scrabble = Scrabble.new("hello")
+    assert_equal ["h", "e", "l", "l", "o"], scrabble.score
   end
 end
