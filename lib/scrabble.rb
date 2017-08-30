d63d5fc1bd63e5a1ce8b1b190bf879f2d031212a
seoulstice <jungyoungjoon@gmail.com>
2017-08-30 09:55:22 -0600

    Commit for evaluation

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,31 +1,35 @@
 class Scrabble
   require 'pry'
-
-  def score("word")
-    skip
+  attr_reader :word,
+              :point_values
+  def initialize
+    @word = word
+    @point_values = {
+        "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+        "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+        "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+        "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+        "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+        "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+        "Y"=>4, "Z"=>10
+      }
   end
 
-  def word_split("word")
-    letters = word.split(//)
-    letters.each do |letter|
+  def score(word)
+    score = []
+    if word == ""
+      return 0
+    elsif word.nil?
+      return 0
+    else
+      upcased = word.upcase
+      split_word = upcased.split(//)
+      split_word.map do |letter|
+        score << @point_values[letter]
+      end
+      return score.inject(:+)
     end
-    puts letter
   end
 
-  # need score_with_multipliers method
-
-  def point_values
-    hash = {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
-    }
-
-  end
-  binding.pry
-
+  
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -1,24 +1,20 @@
 gem 'minitest'
-require_relative '../lib/scrabble'
+require './lib/scrabble'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
-  skip
   def test_it_exists
     scrabble = Scrabble.new
-  end
 
+    assert_instance_of Scrabble, scrabble
+  end
 
   def test_it_can_score_a_single_letter
-    skip
-    assert_equal 1, Scrabble.new.score("a")
-    assert_equal 4, Scrabble.new.score("f")
-  end
+    scrabble = Scrabble.new
 
-  def test_it_splits_word_into_array
-    scrabble = Scrabble.new("hello")
-    assert_equal ["h", "e", "l", "l", "o"], scrabble.score
+    assert_equal 1, scrabble.score("a")
+    assert_equal 4, scrabble.score("f")
   end
 end
